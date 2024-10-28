73f0383885c8c9759572f9d772f706c4f012244b
Ben <ben24france@gmail.com>
2024-10-28 12:33:00 +0100

    v1.1

--- date02.js
+++ date02.js
@@ -3,20 +3,19 @@
 function isValidDate(dateString) {
     const date = new Date(dateString);
     const year = date.getFullYear();
-    const month = date.getMonth() +1;
-    const day = date.getDate();
+    let month = date.getMonth() +1;
+    let day = date.getDate();
     if (month < 10){
-        "0"+month
+        month = "0"+month
     }
     if (day < 10){
-        "0"+day
+        day = "0"+day
     }
     compare = year+"-"+month+"-"+day
-    return compare
     if (compare === dateString) {
         return true
     }
     return false
 }
 
-console.log(isValidDate("2023-02-03"))
+console.log(isValidDate("2023-02-29"))
